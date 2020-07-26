9b1895ba3fc512a547d429de7624f68fdeb1670d
Javier <jc9498863@gmail.com>
2020-07-25 20:26:07 -0500

    creacion de rutas y iconos de iniciode sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Router }from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'principal', component: PrincipalComponent},
+  {path: 'categorias', component: CategoriasComponent},
+  {path: '**', pathMatch: 'full', redirectTo:'principal'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
